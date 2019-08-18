8571d5da97e42322fdce88dbb3b21c9f7bb4c357
mora <14041299@itdurango.edu.mx>
2019-08-18 16:34:15 -0500

    creación de index y nuevo

---
+++ Controllers/tablaController.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MVCCRUD.Models;
+using MVCCRUD.Models.ViewModels;
+
+namespace MVCCRUD.Controllers
+{
+    public class tablaController : Controller
+    {
+        // GET: tabla
+        public ActionResult Index()
+        {
+            List<ListTablaViewModel> lst;
+            using (crudEntities db = new crudEntities())
+            {
+                 lst = (from d in db.tabla
+                           select new ListTablaViewModel
+                           {
+                               Id = d.id,
+                               Nombre = d.nombre,
+                               Correo = d.correo,
+                            //   Fecha_Nacimiento = Convert.ToDateTime("fecha_nacimiento")
+
+
+            }).ToList();
+
+            }
+
+
+            return View(lst);
+        }
+
+        public ActionResult Nuevo()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Nuevo(tablaViewModel model)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    using (crudEntities db = new crudEntities())
+                    {
+                        var oTabla = new tabla();
+                        oTabla.correo = model.Correo;
+                        oTabla.fecha_nacimiento = model.Fecha_Nacimiento;
+                        oTabla.nombre = model.Nombre;
+
+                        db.tabla.Add(oTabla);
+                        db.SaveChanges();
+                    }
+                    return Redirect("/");
+
+                }
+                return View(model);
+            }
+            catch (Exception ex)
+            {
+
+                throw new Exception(ex.Message);
+            }
+        }
+
+    }
+}
---
+++ Models/ViewModels/tablaViewModel.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace MVCCRUD.Models.ViewModels
+{
+    public class tablaViewModel
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        [Display(Name ="Nombre")]
+        public string Nombre { get; set; }
+        [Required]
+        [StringLength(50)]
+        [EmailAddress]
+        [Display(Name ="Correo Electronico")]
+        public string Correo { get; set; }
+        [Required]
+        [DataType(DataType.Date)]
+        [Display(Name = "Fecha de Nacimiento")]
+        public DateTime Fecha_Nacimiento { get; set; }
+
+    }
+}
